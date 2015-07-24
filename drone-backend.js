e2e59c1db52ffea57b1afe7661ea45ff9d5d86ce
V3501 (Conference ID) <v3501@cam.ac.uk>
2015-07-24 12:56:24 +0100

    drone test v5

--- drone-backend.js
+++ drone-backend.js
@@ -26,26 +26,26 @@ function fly(robot) {
     bot.drone.up(1);
     after( 1000, function() {
         bot.drone.front(maxSpeed);
-        bot.drone.left(0.35);
+        bot.drone.right(0.35);
     });
 
-    after(3 * 1000, function () {
+    after(3.2 * 1000, function () {
         bot.drone.stop();
-        bot.drone.right(maxSpeed)
+        bot.drone.left(maxSpeed)
     });
-    after(4.35 * 1000, function() {
+    after(5.55 * 1000, function() {
         bot.drone.stop();
         bot.drone.back(maxSpeed);
-        bot.drone.left(0.35);
+        bot.drone.right(0.35);
 
     });
-    after(6.35 * 1000, function() {
+    after(8.75 * 1000, function() {
         bot.drone.down(1)
     });
-    after(7.35 * 1000, function(){
+    after(9.75 * 1000, function(){
         bot.drone.land()
     });
-    after(8.35 * 1000, function () {
+    after(10.75 * 1000, function () {
         bot.drone.stop();
 
     });
