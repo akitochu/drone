13414d4b4069f168e8f634fc7737d7d1a7051efe
V3501 (Conference ID) <v3501@cam.ac.uk>
2015-07-24 11:00:51 +0100

    drone test

--- drone-backend.js
+++ drone-backend.js
@@ -1,6 +1,6 @@
 var Cylon = require('cylon');
 var ws = require('nodejs-websocket');
-var bot;
+
 
 // Initialise the robot
 Cylon.robot()
@@ -14,9 +14,52 @@ Cylon.robot()
     })
     .on("ready", fly);
     
+
 // Fly the bot
+var bot;
+var maxSpeed = 1;
 function fly(robot) {
+    bot = robot;
+    bot.drone.disableEmergency();
+    bot.drone.ftrim();
+    bot.drone.takeoff();
+    front(maxSpeed);
+    left(0.43);
+    up(0.1);
+
+    after(5.2 * 1000, function () {
+        left(0);
+        front(0);
+        up(0);
+        right(maxSpeed)
+    });
+    after(3.6 * 1000, function() {
+        right(0);
+        back(maxSpeed);
+        left(0.43);
+        down(0.1)
+    });
+    after(5.2 * 1000, function(){
+        bot.drone.land()
+    });
+    afterfter(7.5 * 1000, function () {
+        bot.drone.stop();
+
+    });
+    bot.nav.on("navdata", function (data) {
+        console.log(data);
+    });
+    bot.nav.on("batteryChange", function(data) {
+        console.log("Battery level:", data);
+    });
+    bot.nav.on("altitudeChange", function(data) {
+        console.log("Altitude:", data);
+        if (altitude > 1.5) {
+            bot.drone.land();
+        }
+    });
 
 }
 
 Cylon.start();
+
